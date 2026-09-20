8e4b40d06b9efbfec9f845dd50222e02e197bdfa
vladislav1212d <vladi_penev@abv.bg>
2026-09-20 14:26:30 +0300

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class EasterEgssBattle
+{
+    static void Main()
+    {
+      int playerOneEggs = int.Parse(Console.ReadLine());
+      int playerTwoEggs = int.Parse(Console.ReadLine());
+
+        while (true)
+        {
+          string winner = Console.ReadLine();
+
+          if (winner == "one")
+            {
+              playerTwoEggs--;
+              if (playerTwoEggs < 1)
+                {
+                    Console.WriteLine($"Player two is out of eggs. Player one has {playerOneEggs} eggs left.");
+                    return;
+                }
+            }
+            else if (winner == "two")
+            {
+                playerOneEggs--;
+                if (playerOneEggs < 1)
+                {
+                    Console.WriteLine($"Player one is out of eggs. Player two has {playerTwoEggs} eggs left.");
+                    return;
+                }
+            }
+            else if (winner == "End")
+            {
+                Console.WriteLine($"Player one has {playerOneEggs} eggs left.");
+                Console.WriteLine($"Player two has {playerTwoEggs} eggs left.");
+                return;
+            }
+        }
+    }
+}
+
